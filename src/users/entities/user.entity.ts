f65fd7a4dfc4490f985fb2dfc5f06f38943b3018
Brian Lee <brian971002@gmail.com>
2024-07-10 17:00:18 -0400

    fix: creates user role as enums instead of a string value

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,4 +1,4 @@
-import { Field, InputType, ObjectType } from "@nestjs/graphql";
+import { Field, InputType, ObjectType, registerEnumType } from "@nestjs/graphql";
 import { string } from "joi";
 import { CoreEntity } from "src/common/entities/core.entity";
 import { Column, Entity } from "typeorm";
@@ -10,7 +10,13 @@ import { Column, Entity } from "typeorm";
     - password
 */
 
-type UserRole = "client" | "owner" | "delivery"
+enum UserRole {
+    Owner,
+    Client,
+    Delivery
+}
+
+registerEnumType(UserRole, { name: "UserRole" })
 
 @InputType({ isAbstract: true })
 @ObjectType()
@@ -18,14 +24,14 @@ type UserRole = "client" | "owner" | "delivery"
 export class User extends CoreEntity {
 
     @Column()
-    @Field(type=> String)
+    @Field(type => String)
     email: string
 
     @Column()
-    @Field(type=> String)
+    @Field(type => String)
     password: string
 
-    @Column()
-    @Field(type=> String)
+    @Column({ type: 'enum', enum: UserRole })
+    @Field(type => UserRole)
     role: UserRole
 }
--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,15 +1,22 @@
-import { Query, Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { User } from "./entities/user.entity";
 import { UsersService } from "./users.service";
+import { CreateAccountInput, CreateAccountOutput } from "./dtos/create_account.dto";
 
 
 @Resolver(of => User)
 export class UsersResolver {
+
     constructor(
         private readonly usersService: UsersService
-    ){}
-    @Query(returns=>Boolean)
-    hi(){
+    ) { }
+
+    @Query(returns => Boolean)
+    hi() {
         return true
     }
+
+    @Mutation(returns => CreateAccountOutput)
+    createAccount(@Args("input") createAccountInput: CreateAccountInput){}
+
 }
