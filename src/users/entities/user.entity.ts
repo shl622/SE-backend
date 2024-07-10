89d923533bbcd697d640e0db866565825ed15afa
Brian Lee <brian971002@gmail.com>
2024-07-10 16:50:50 -0400

    feat: adds graphQL fields for User and core entities

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,12 +1,9 @@
+import { Field, InputType, ObjectType } from "@nestjs/graphql";
+import { string } from "joi";
 import { CoreEntity } from "src/common/entities/core.entity";
 import { Column, Entity } from "typeorm";
 
 /*
-    global entity ( in common module )
-    - id
-    - createdAt
-    - updatedAt
-
     user-specific entity
     - email
     - role (client, owner, delivery)
@@ -15,14 +12,20 @@ import { Column, Entity } from "typeorm";
 
 type UserRole = "client" | "owner" | "delivery"
 
+@InputType({ isAbstract: true })
+@ObjectType()
 @Entity()
-export class User extends CoreEntity{
+export class User extends CoreEntity {
+
     @Column()
+    @Field(type=> String)
     email: string
 
     @Column()
+    @Field(type=> String)
     password: string
 
     @Column()
+    @Field(type=> String)
     role: UserRole
 }
