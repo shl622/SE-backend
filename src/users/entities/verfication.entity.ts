12046673a3d2cb436c97ccb9c559c4431a52076d
Brian Lee <brian971002@gmail.com>
2024-07-16 11:41:15 -0400

    fix: adds cascade relatinoship for ondelete of user<>verification

--- src/users/entities/verfication.entity.ts
+++ src/users/entities/verfication.entity.ts
@@ -17,7 +17,7 @@ export class Verification extends CoreEntity {
     @Field(type=>String)
     code: string
 
-    @OneToOne(type=>User)
+    @OneToOne(type=>User, {onDelete:"CASCADE"})
     @JoinColumn()
     user: User
 
