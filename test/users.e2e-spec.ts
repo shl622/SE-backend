9ebf6f0acc78e2f23722dff023fae333bcd50f11
Brian Lee <brian971002@gmail.com>
2024-07-20 11:35:50 -0400

    test: tests graphQL endpoint for createAccount and fixes issue of email being sent on every test

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -4,6 +4,13 @@ import { AppModule } from '../src/app.module';
 import { DataSource } from 'typeorm';
 import * as request from 'supertest'
 
+//mock fetch so email is not sent on every test
+jest.mock("node-fetch", () =>{
+  return{
+    fetch: jest.fn()
+  }
+})
+
 const GRAPHQL_ENDPOINT = '/graphql'
 
 describe('UserModule (e2e)', () => {
@@ -70,7 +77,29 @@ describe('UserModule (e2e)', () => {
         expect(res.body.data.createAccount.error).toBe(null)
       })
     })
-    it.todo('should fail if account already exists')
+    it('should fail if account already exists',()=>{
+      return request(app.getHttpServer())
+      .post(GRAPHQL_ENDPOINT)
+      .send({
+        query: `
+        mutation{
+          createAccount(input:{
+            email:"${EMAIL}",
+            password:"12345",
+            role:Owner
+          }) {
+            ok
+            error
+          }
+        }
+        `,
+      })
+      .expect(200)
+      .expect(res=>{
+        expect(res.body.data.createAccount.ok).toBe(false)
+        expect(res.body.data.createAccount.error).toEqual(expect.any(String))
+      })
+    })
   })
 
 
