d20104c6871be7013d5cef4ed894e456e6e8e56c
Brian Lee <brian971002@gmail.com>
2024-07-20 11:29:44 -0400

    test: e2e tests creating account but has issue of email being sent on every test

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -2,6 +2,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import { AppModule } from '../src/app.module';
 import { DataSource } from 'typeorm';
+import * as request from 'supertest'
+
+const GRAPHQL_ENDPOINT = '/graphql'
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
@@ -15,6 +18,7 @@ describe('UserModule (e2e)', () => {
     await app.init();
   });
 
+  //for every test, drop the DB used for E2E testing
   afterAll(async () => {
     const dataSource = new DataSource({
       type: "postgres",
@@ -23,19 +27,57 @@ describe('UserModule (e2e)', () => {
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      });
-      const connection = await dataSource.initialize();
-      await connection.dropDatabase();
-      await connection.destroy();
-      await app.close();
+    });
+    const connection = await dataSource.initialize();
+    await connection.dropDatabase(); //delete test db
+    await connection.destroy(); //disconnect
+    await app.close();
     app.close()
   })
 
-  it.todo('currAuth')
+  /*
+  to-do:
+  1. test creating account
+  2. test if id of any user is reached
+  3. test logging in
+  4. test current logged in user
+  5. verify email of user
+  6. check if edits user profile
+  */
+
+  describe('createAccount', () => {
+    const EMAIL = "test@e2e.com"
+    it('should create account', () => {
+      return request(app.getHttpServer())
+      .post(GRAPHQL_ENDPOINT)
+      .send({
+        query: `
+        mutation{
+          createAccount(input:{
+            email:"${EMAIL}",
+            password:"12345",
+            role:Owner
+          }) {
+            ok
+            error
+          }
+        }
+        `,
+      })
+      .expect(200)
+      .expect(res=>{
+        expect(res.body.data.createAccount.ok).toBe(true)
+        expect(res.body.data.createAccount.error).toBe(null)
+      })
+    })
+    it.todo('should fail if account already exists')
+  })
+
+
   it.todo('userProfile')
-  it.todo('createAccount')
   it.todo('login')
-  it.todo('editProfile')
+  it.todo('currAuth')
   it.todo('verifyEmail')
+  it.todo('editProfile')
 
 });
