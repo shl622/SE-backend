174489d212299c981dbe93dd271b8f44c5a816ac
Brian Lee <brian971002@gmail.com>
2024-09-09 09:28:50 -0400

    refactor: separate options from the order and creates relation with each dish for options

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,15 +1,26 @@
 import { Field, InputType, Int, ObjectType, PickType } from "@nestjs/graphql";
 import { CoreOutput } from "src/common/dto/output.dto";
-import { Order } from "../entities/order.entity";
-
+import { DishOption } from "src/restaurants/entities/dish.entity";
 
 @InputType()
-export class CreateOrderInput extends PickType(Order, ["items"]){
-    @Field(type=>Int)
+class CreateOrderItemInput {
+    @Field(type => Int)
+    dishId: number
+
+    @Field(type => DishOption, { nullable: true })
+    options?: DishOption[]
+
+}
+@InputType()
+export class CreateOrderInput {
+    @Field(type => Int)
     restaurantId: number
+
+    @Field(type => [CreateOrderItemInput])
+    items: CreateOrderItemInput[]
 }
 
 @ObjectType()
-export class CreateOrderOutput extends CoreOutput{
+export class CreateOrderOutput extends CoreOutput {
 
 }
